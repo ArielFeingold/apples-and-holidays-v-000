c0b23b0aa81ffb3f2ba89d9edf7d7f88c5fea94e
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-15 18:04:40 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,6 +53,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday_array|
     puts "#{season.capitalize}:"
     puts "  #{season.key}"
+    puts "#{season.value}"
   end
 end
 

a4e99ac74bf41beeada42b2fac01e23e3ad6c028
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-15 20:24:52 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ def all_supplies_in_holidays(holiday_hash)
       if holiday.to_s.include?("_")
         puts "  #{holiday.to_s.tr("_", " ").capitalize}: #{supplies.join(", ")}"
       else
-        puts "  #{holiday.to_s.split('_').collect {|w| w.capitalize}.join(' ')}: #{supplies.join(", ")}"
+        puts "  #{holiday.to_s.split('_').collect{|w| w.capitalize}.join(' ')}: #{supplies.join(", ")}"
       end
     end
   end

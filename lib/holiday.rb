b5bb261333c68a44322b5c22229930f610e4be1b
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-15 15:56:25 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,6 +55,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     puts " #{holiday.join}:"
   end
+  end
 end
 
 # iterate through holiday_hash and print items such that your readout resembles:

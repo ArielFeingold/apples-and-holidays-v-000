5c2cc6a85812f53de1d077e4dd765f42cc1bb6bc
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-15 15:47:42 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,7 +51,6 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
-    holiday_string = holiday.join
     puts "#{season.capitalize}:"
     puts "#{holiday_string}:"
   end
